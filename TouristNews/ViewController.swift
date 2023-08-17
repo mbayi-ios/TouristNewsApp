4a0baa94148e09ac48fcc8d37f5966d423e97dc0
Ambrose Mbayi <91916741+mbayi-ios@users.noreply.github.com>
2023-08-17 21:06:16 +0300

    Initial Commit

---
+++ TouristNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TouristNews
+//
+//  Created by Ambrose Mbayi on 17/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
